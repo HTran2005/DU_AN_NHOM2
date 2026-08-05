85fa368b14ceef98fc40208a26535f8c55105bb9
Dungcute <0023412291@student.dthu.edu.com>
2026-08-05 13:08:43 +0700

    helo

--- backend/user.php
+++ backend/user.php
@@ -52,7 +52,10 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // Import config
 require_once __DIR__ . '/config.php';
 
