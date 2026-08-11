3290245f7f01a5efb9738387068e8df8f98cc589
NHOM2 <tran09102005@an09102005gmail.>
2026-08-11 15:14:25 +0700

    configure redis connection

--- backend/redis.php
+++ backend/redis.php
@@ -1,16 +1,7 @@
-
-# <?php
-
-# return [
-   # 'host' => $_ENV['REDIS_HOST'] ?? '',
-    #'port' => (int)($_ENV['REDIS_PORT'] ?? 10000),
-    #'password' => $_ENV['REDIS_PASSWORD'] ?? '',
-#];
-
-<?php
-
-return [
-    'host' => getenv('REDIS_HOST') ?: '',
-    'port' => (int)(getenv('REDIS_PORT') ?: 10000),
-    'password' => getenv('REDIS_PASSWORD') ?: '',
+ <?php
+ return [
+    'host' => $_ENV['REDIS_HOST'] ?? '',
+    'port' => (int)($_ENV['REDIS_PORT'] ?? 10000),
+    'password' => $_ENV['REDIS_PASSWORD'] ?? '',
 ];
+
